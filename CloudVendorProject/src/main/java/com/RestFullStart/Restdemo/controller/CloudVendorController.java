a008bb3152e1e52f3fc0614302d4e819236a8e4b
RaghvendraCodeForce <raghvendra@thescalers.com>
2025-04-21 01:09:04 +0530

    Vendor API custom Response Added

--- CloudVendorProject/src/main/java/com/RestFullStart/Restdemo/controller/CloudVendorController.java
+++ CloudVendorProject/src/main/java/com/RestFullStart/Restdemo/controller/CloudVendorController.java
@@ -2,6 +2,8 @@ package com.RestFullStart.Restdemo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.RestFullStart.Restdemo.Modal.CloudVendor;
 import com.RestFullStart.Restdemo.Service.CloudVendorService;
+import com.RestFullStart.Restdemo.response.ResponseHandler;
 
 // Rest controller mark the class and tell that this class is responsible for API calls
 
@@ -31,9 +34,10 @@ public class CloudVendorController {
 
 	// Read Specific Cloud Vendor Details
 	@GetMapping("{vendorId}")
-	public CloudVendor getCloudVendorDetails(@PathVariable String vendorId)
+	public ResponseEntity<Object> getCloudVendorDetails(@PathVariable("vendorId") String vendorId)
 	{
-		return cloudVendorService.getCloudVendor(vendorId);//new CloudVendor("c1","vendor1","Address one","xxxxxx");
+		return ResponseHandler.responseBuilder("Requested vendor details are given here ", HttpStatus.OK, cloudVendorService.getCloudVendor(vendorId));
+		//new CloudVendor("c1","vendor1","Address one","xxxxxx");
 	}
 	
 	// Read all Cloud Vendor Details
---
+++ CloudVendorProject/src/main/java/com/RestFullStart/Restdemo/response/ResponseHandler.java
@@ -0,0 +1,23 @@
+package com.RestFullStart.Restdemo.response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHandler {
+
+	public static ResponseEntity<Object> responseBuilder(
+			String message, HttpStatus httpStatus, Object responseObject
+			)
+	{
+		Map<String,Object> response = new HashMap<>();
+		response.put("Message", message);
+		response.put("HttpStatus", httpStatus);
+		response.put("data", responseObject);
+		
+		return new ResponseEntity<>(response,httpStatus);
+	}
+	
+}
